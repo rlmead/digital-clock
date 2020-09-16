6509d18172241ad0f11c0a845abc4c42750897bf
Rebecca Mead <beckyleemead@gmail.com>
2020-09-15 22:02:51 -0400

    add functionality for clock to display inaccurate times when it's feeling down

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
+// define constants
 const clock = document.querySelector('div');
+const weather = 'sunny';
 
-setInterval(function() { clock.textContent = Date().split(' ')[4]; }, 1000 )
+// create zero-padding function to make the randomized clock look more cohesive
+function zero_pad(input_string, desired_length) {
+    let output_string = input_string;
+    while (output_string.length < desired_length) {
+        output_string = '0' + output_string;
+    }
+    return output_string;
+}
+
+if (weather === 'sunny') {
+    setInterval(function () { clock.textContent = Date().split(' ')[4]; }, 1000);
+} else {
+    setInterval(function () {
+        let hours = zero_pad((Math.floor(Math.random() * 12) + 1).toString(),2);
+        let minutes = zero_pad(Math.floor(Math.random() * 60).toString(),2);
+        let seconds = zero_pad(Math.floor(Math.random() * 60).toString(),2);
+        clock.textContent = hours + ':' + minutes + ':' + seconds;
+    }, 1000)
+}
 
 // background colors to alternate:
 // gloomy: #7A7A7A;
