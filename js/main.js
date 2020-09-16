5c84c527c5b57af40c87281af40269cf78d1b844
Rebecca Mead <beckyleemead@gmail.com>
2020-09-16 06:59:57 -0400

    minor layout and code improvements

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // define constants
 const weather = 'sunny';
 const body = document.querySelector('body');
-const clock = document.querySelector('div');
-const message1 = document.querySelector('h1')
-const message2 = document.querySelector('h2')
+const clock = document.getElementById('clock');
+const emoticon = document.getElementById('emoticon');
+const message = document.getElementById('message');
 
-// create zero-padding function to make the randomized clock look more cohesive
+// define zero-padding function to make the randomized clock look more cohesive
 function zero_pad(input_string, desired_length) {
     let output_string = input_string;
     while (output_string.length < desired_length) {
@@ -14,11 +14,13 @@ function zero_pad(input_string, desired_length) {
     return output_string;
 }
 
+// define function to show accurate time
 function show_accurate_time() {
     let [hours, minutes, seconds] = (new Date()).toLocaleTimeString().slice(0, 8).split(':');
-    clock.textContent = hours + ':' + minutes + ':' + seconds;
+    clock.textContent = zero_pad(hours, 2) + ':' + minutes + ':' + seconds;
 }
 
+// define function to show inaccurate time
 function show_inaccurate_time() {
     let hours = zero_pad((Math.floor(Math.random() * 12) + 1).toString(), 2);
     let minutes = zero_pad(Math.floor(Math.random() * 60).toString(), 2);
@@ -26,26 +28,27 @@ function show_inaccurate_time() {
     clock.textContent = hours + ':' + minutes + ':' + seconds;
 }
 
+// set the clock's mood according to the weather
 if (weather === 'sunny') {
+    // format page according to mood
     body.setAttribute('style', 'background-color: #87CEEB');
     clock.setAttribute('class', 'happy');
-    message1.setAttribute('style', 'font-family: \'Happy Monkey\', sans-serif');
-    message1.textContent = 'HEY YOU ᕕ( ᐛ )ᕗ';
-    message2.setAttribute('style', 'font-family: \'Happy Monkey\', sans-serif');
-    message2.textContent = 'it\'s a great time to do something fun';
+    emoticon.textContent = 'ᕕ( ᐛ )ᕗ';
+    message.setAttribute('style', 'font-family: \'Happy Monkey\', sans-serif');
+    message.textContent = 'it\'s a great time to do something fun!';
+    // show the time immediately upon loading
     show_accurate_time();
+    // change the time every second
     setInterval(function () { show_accurate_time() }, 1000);
 } else {
+    // format page according to mood
     body.setAttribute('style', 'background-color: #7A7A7A');
     clock.setAttribute('class', 'gloomy');
-    message1.setAttribute('style', 'font-family: \'Special Elite\', sans-serif');
-    message1.textContent = 'meh ಠ╭╮ಠ';
-    message2.setAttribute('style', 'font-family: \'Special Elite\', sans-serif');
-    message2.textContent = 'i don\'t wanna tell you what time it is';
+    emoticon.textContent = 'ಠ╭╮ಠ';
+    message.setAttribute('style', 'font-family: \'Special Elite\', sans-serif');
+    message.textContent = 'i don\'t wanna tell you what time it is';
+    // show the time immediately upon loading
     show_inaccurate_time();
+    // change the time every second
     setInterval(function () { show_inaccurate_time() }, 1000);
 }
-
-// background colors to alternate:
-// gloomy: #7A7A7A;
-// happy: #87CEEB;
