e25430927cf63b4a65b69243fc40179ae08a5991
Rebecca Mead <beckyleemead@gmail.com>
2020-09-16 07:34:27 -0400

    add button to toggle between moods

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // define constants
-const weather = 'sunny';
+let weather = 'sunny';
 const body = document.querySelector('body');
 const clock = document.getElementById('clock');
 const emoticon = document.getElementById('emoticon');
 const message = document.getElementById('message');
+const button = document.querySelector('button');
 
 // define zero-padding function to make the randomized clock look more cohesive
 function zero_pad(input_string, desired_length) {
@@ -28,27 +29,45 @@ function show_inaccurate_time() {
     clock.textContent = hours + ':' + minutes + ':' + seconds;
 }
 
+let running_clock;
+
 // set the clock's mood according to the weather
-if (weather === 'sunny') {
-    // format page according to mood
-    body.setAttribute('style', 'background-color: #87CEEB');
-    clock.setAttribute('class', 'happy');
-    emoticon.textContent = 'ᕕ( ᐛ )ᕗ';
-    message.setAttribute('style', 'font-family: \'Happy Monkey\', sans-serif');
-    message.textContent = 'it\'s a great time to do something fun!';
-    // show the time immediately upon loading
-    show_accurate_time();
-    // change the time every second
-    setInterval(function () { show_accurate_time() }, 1000);
-} else {
-    // format page according to mood
-    body.setAttribute('style', 'background-color: #7A7A7A');
-    clock.setAttribute('class', 'gloomy');
-    emoticon.textContent = 'ಠ╭╮ಠ';
-    message.setAttribute('style', 'font-family: \'Special Elite\', sans-serif');
-    message.textContent = 'i don\'t wanna tell you what time it is';
-    // show the time immediately upon loading
-    show_inaccurate_time();
-    // change the time every second
-    setInterval(function () { show_inaccurate_time() }, 1000);
+function change_mood(weather) {
+    if (weather === 'sunny') {
+        // format page according to mood
+        body.setAttribute('style', 'background-color: #87CEEB');
+        clock.setAttribute('class', 'happy');
+        emoticon.textContent = 'ᕕ( ᐛ )ᕗ';
+        message.setAttribute('style', 'font-family: \'Happy Monkey\', sans-serif');
+        message.textContent = 'it\'s a great time to do something fun!';
+        button.textContent = 'it\'s gloomy out';
+        // show the time immediately upon loading
+        show_accurate_time();
+        // change the time every second
+        running_clock = setInterval(function () { show_accurate_time() }, 1000);
+    } else {
+        // format page according to mood
+        body.setAttribute('style', 'background-color: #7A7A7A');
+        clock.setAttribute('class', 'gloomy');
+        emoticon.textContent = 'ಠ╭╮ಠ';
+        message.setAttribute('style', 'font-family: \'Special Elite\', sans-serif');
+        message.textContent = 'i don\'t wanna tell you what time it is';
+        button.textContent = 'it\'s sunny out';
+        // show the time immediately upon loading
+        show_inaccurate_time();
+        // change the time every second
+        running_clock = setInterval(function () { show_inaccurate_time() }, 1000);
+    }
 }
+
+change_mood(weather);
+
+button.onclick = function () {
+    if (weather === 'sunny') {
+        weather = 'gloomy';
+    } else {
+        weather = 'sunny';
+    }
+    clearInterval(running_clock);
+    change_mood(weather);
+};
