1bce169e689c6e6df20a56beb6d24a6fbafae9ba
Rebecca Mead <beckyleemead@gmail.com>
2020-09-15 22:27:07 -0400

    add mood formatting and make clock appear immediately upon loading

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // define constants
-const clock = document.querySelector('div');
 const weather = 'sunny';
+const body = document.querySelector('body');
+const clock = document.querySelector('div');
+const message = document.querySelector('h2')
 
 // create zero-padding function to make the randomized clock look more cohesive
 function zero_pad(input_string, desired_length) {
@@ -11,18 +13,30 @@ function zero_pad(input_string, desired_length) {
     return output_string;
 }
 
+function show_accurate_time() {
+    let [hours, minutes, seconds] = (new Date()).toLocaleTimeString().slice(0, 8).split(':');
+    clock.textContent = hours + ':' + minutes + ':' + seconds;
+}
+
+function show_inaccurate_time() {
+    let hours = zero_pad((Math.floor(Math.random() * 12) + 1).toString(), 2);
+    let minutes = zero_pad(Math.floor(Math.random() * 60).toString(), 2);
+    let seconds = zero_pad(Math.floor(Math.random() * 60).toString(), 2);
+    clock.textContent = hours + ':' + minutes + ':' + seconds;
+}
+
 if (weather === 'sunny') {
-    setInterval(function() {
-        let [hours, minutes, seconds] = (new Date()).toLocaleTimeString().slice(0,8).split(':');
-        clock.textContent = hours + ':' + minutes + ':' + seconds;
-    }, 1000)
+    body.setAttribute('style', 'background-color: #87CEEB');
+    clock.setAttribute('class', 'happy');
+    message.textContent = 'HEY YOU, it\'s a great time to do something fun ᕕ( ᐛ )ᕗ';
+    show_accurate_time();
+    setInterval(function () { show_accurate_time() }, 1000);
 } else {
-    setInterval(function () {
-        let hours = zero_pad((Math.floor(Math.random() * 12) + 1).toString(),2);
-        let minutes = zero_pad(Math.floor(Math.random() * 60).toString(),2);
-        let seconds = zero_pad(Math.floor(Math.random() * 60).toString(),2);
-        clock.textContent = hours + ':' + minutes + ':' + seconds;
-    }, 1000)
+    body.setAttribute('style', 'background-color: #7A7A7A');
+    clock.setAttribute('class', 'gloomy');
+    message.textContent = 'i don\'t wanna tell you what time it is ಠ╭╮ಠ';
+    show_inaccurate_time();
+    setInterval(function () { show_inaccurate_time() }, 1000);
 }
 
 // background colors to alternate:
